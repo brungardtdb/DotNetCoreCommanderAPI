b8bb33040060ae03d0261432b82baf9f65291149
David Brungardt <brungardtdb@gmail.com>
2021-03-12 09:14:32 -0700

    Committing Before Merging with Main

--- Models/Command.cs
+++ Models/Command.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.Models
 {
     public class Command
     {
+        [Key] // Key for DB table
         public int Id {get; set;}
+        
+        [Required] // Cannot be null
+        [MaxLength(50)] // We can also limit characters
         public string HowTo {get; set;}
+
+        [Required]
         public string Line {get; set;}
+
+        [Required]
         public string Platform {get; set;}
     }
 }
