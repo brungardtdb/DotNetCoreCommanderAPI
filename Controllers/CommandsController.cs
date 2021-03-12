877299bf78ca166a5c59eb693e737a89d41e0b9e
David Brungardt <brungardtdb@gmail.com>
2021-03-11 18:08:48 -0700

    Set Up DI for Repository
    Set up dependency injection for repository and injected mock repository.

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Commander.Data;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,16 +10,27 @@ namespace Commander.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
+        private readonly ICommanderRepo _repository;
+
+        public CommandsController(ICommanderRepo repository)
+        {
+            _repository = repository;
+        }
+
+        //GET api/commands
         [HttpGet]
         public ActionResult <IEnumerable<Command>> GetAllCommands()
         {
-
+            var commandItems = _repository.GetAllCommands();
+            return Ok(commandItems); // Return OK 200 success
         }
 
-        [HttpGet]
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
         public ActionResult <Command> GetCommandById(int id)
         {
-
+            var commandItem = _repository.GetCommandById(1);
+            return Ok(commandItem); // Return OK 200 success
         }
     }
 }
